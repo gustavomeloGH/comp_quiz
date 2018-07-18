c6a0a339ed398a4faa9a7edda2191193276333ad
Luiz Felix <phellykz-020@hotmail.com>
2018-07-18 09:41:10 -0300

    Delete firebase.config.ts

--- src/environments/firebase.config.ts
+++
@@ -1,8 +0,0 @@
-export const FirebaseConfig = {
-    apiKey: "AIzaSyBuoO7HK_0nVhfFZCOvgdDd6pf7_GhV1VU",
-    authDomain: "compquiz-f8a29.firebaseapp.com",
-    databaseURL: "https://compquiz-f8a29.firebaseio.com",
-    projectId: "compquiz-f8a29",
-    storageBucket: "",
-    messagingSenderId: "177811563148"
-};
